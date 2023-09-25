5e0a401d824691d56b62a451a619710363d535c2
MichaelHeinzman <seth@heinzman.net>
2023-09-24 21:47:00 -0400

    fixed series bug

--- src/lib/scoreboard/ScoreboardSeriesBox.svelte
+++ src/lib/scoreboard/ScoreboardSeriesBox.svelte
@@ -2,17 +2,14 @@
     import { series } from "../processor";
     export let team = 0;
     let left = team === 0;
-    let best_of = $series?.best_of || null;
-    let team_score = $series?.[team].score;
 
     // Calculate the number of lines needed to win
-    let linesToWin = Math.ceil(best_of / 2);
 </script>
 
 <div class="team_series_box {left ? 'left_team_series_box' : 'right_team_series_box'}">
-    {#if best_of}
-        {#each Array(linesToWin) as _, index}
-            {#if index < team_score}
+    {#if $series?.best_of}
+        {#each Array(Math.ceil($series?.best_of / 2)) as _, index}
+            {#if index < $series?.[team].score}
                 <div class="team_series_point"/>
             {:else}
                 <div class="team_series_point team_series_point_empty"/>
